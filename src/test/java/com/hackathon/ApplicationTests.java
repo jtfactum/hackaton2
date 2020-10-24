3c06106220d54087e9c73530e8baac72e2ee92e6
Eugene <jtfactum@gmail.com>
2020-10-24 13:57:21 +0300

    refactoring

--- src/test/java/com/hackathon/ApplicationTests.java
+++ src/test/java/com/hackathon/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.HakatonSpring;
+package com.hackathon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HakatonSpringApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
