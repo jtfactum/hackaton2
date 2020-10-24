8194a1beeed451526e97651b9d8309d12efafb3c
Eugene <jtfactum@gmail.com>
2020-10-24 13:57:21 +0300

    refactoring

--- src/main/java/com/hackathon/Application.java
+++ src/main/java/com/hackathon/Application.java
@@ -1,13 +1,13 @@
-package com.example.HakatonSpring;
+package com.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HakatonSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HakatonSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
