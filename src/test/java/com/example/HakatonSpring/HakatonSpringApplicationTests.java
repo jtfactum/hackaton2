be568544f6ca049c588e080847bee1ec37faacc6
Eugene <jtfactum@gmail.com>
2020-10-24 13:23:51 +0300

    add project structure

---
+++ src/test/java/com/example/HakatonSpring/HakatonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HakatonSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakatonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
